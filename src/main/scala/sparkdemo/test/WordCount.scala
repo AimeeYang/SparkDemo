678778f6921f63b220d414166c4d467c60174752
AimeeYang <1014279248@qq.com>
2016-01-27 10:05:14 +0800

    wordcount

---
+++ src/main/scala/sparkdemo/test/WordCount.scala
@@ -0,0 +1,15 @@
+package sparkdemo.test
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+/**
+  * Created by ayang on 2016/1/27.
+  */
+class WordCount {
+
+}
+
+object WordCount{
+  //val conf=new SparkConf().setAppName("WordCount").setMaster()
+
+}
