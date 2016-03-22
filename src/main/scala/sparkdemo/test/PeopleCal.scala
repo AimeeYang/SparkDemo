3d21b3db682ffc8e2459ee962b64d13e0044e911
AimeeYang <1014279248@qq.com>
2016-03-22 14:07:20 +0800

    PeopleCal

--- src/main/scala/sparkdemo/test/PeopleCal.scala
+++ src/main/scala/sparkdemo/test/PeopleCal.scala
@@ -15,11 +15,12 @@ import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.types._
 
 object PeopleCal {
-  val conf=new SparkConf().setAppName("SparkSQL and HiveContext")
-  val sc=new SparkContext(conf)
-  val hiveContext = new org.apache.spark.sql.hive.HiveContext(sc)
-  hiveContext.sql("CREATE TABLE IF NOT EXISTS TestTable2 (key INT, value STRING)")
-  hiveContext.sql("LOAD DATA LOCAL INPATH 'examples/src/main/resources/kv1.txt' INTO TABLE TestTable2")
-  hiveContext.sql("from TestTable2 SELECT key, collect_list(value) group by key order by key").collect.foreach(println)
-
+  def main(arges:Array[String]) {
+    val conf = new SparkConf().setAppName("SparkSQL and HiveContext")
+    val sc = new SparkContext(conf)
+    val hiveContext = new org.apache.spark.sql.hive.HiveContext(sc)
+    hiveContext.sql("CREATE TABLE IF NOT EXISTS TestTable2 (key INT, value STRING)")
+    hiveContext.sql("LOAD DATA LOCAL INPATH 'examples/src/main/resources/kv1.txt' INTO TABLE TestTable2")
+    hiveContext.sql("from TestTable2 SELECT key, collect_list(value) group by key order by key").collect.foreach(println)
+  }
 }
